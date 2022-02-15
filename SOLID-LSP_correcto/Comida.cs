4262629af42e46ade62f7b7ff6cfcec90d06b4e7
Behnder <mercado.nicolas@live.com>
2022-02-15 12:14:02 -0300

    first commit

---
+++ SOLID-LSP_correcto/Comida.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_LSP_correcto
+{
+    class Comida
+    {
+
+        public virtual void PonerIngredientes(String[] ingredientes) { }
+        public virtual void Cocinar() { }
+    }
+
+}
---
+++ SOLID-LSP_correcto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_LSP_correcto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comida comida = new Pizza();
+            comida.PonerIngredientes(new string[] { "jamon", "cebolla", "cantimpalo" });
+            comida.Cocinar();
+            Console.ReadKey();
+        }
+    }
+}
